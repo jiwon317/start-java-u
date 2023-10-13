2ce0896129a9f6f03d677f534c7d32a75d01ba05
jiwon317 <chlghqlt12@naver.com>
2023-10-13 23:28:31 +0900

    VarEx3

---
+++ ch02/src/ch02/VarEx3.java
@@ -0,0 +1,10 @@
+package ch02;
+
+public class VarEx3 {
+	public static void main(String[] args) {
+		final int score = 200;
+//		score = 200;
+//		지역변수는 읽기 전 반드시 초기화를 해야한다.
+		System.out.println(score);
+	}
+}
