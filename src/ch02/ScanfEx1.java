21e3b0a03ea949a83a1c78db6004b532954a1f1d
jiwon317 <chlghqlt12@naver.com>
2023-10-17 23:49:31 +0900

    화면으로부터 입력받기

---
+++ src/ch02/ScanfEx1.java
@@ -0,0 +1,19 @@
+package ch02;
+import java.util.*; // 1. import문 추가
+
+public class ScanfEx1 {
+	public static void main(String[] args) {
+		// 2. Scanner 클래스의 객체 생성
+		Scanner scanner = new Scanner(System.in);
+		
+//		int num = scanner.nextInt();
+//		int num2 = scanner.nextInt();
+//		System.out.println(num);
+//		System.out.println(num2);
+		
+		String input = scanner.nextLine();
+		int num = Integer.parseInt(input);
+		
+		System.out.println(num);
+	}
+}
